c2fdefec6239c6bbfcffe87acd7e43d11bf45c30
KenyaRodriguezm <kenrodriguezkhan@gmail.com>
2024-10-28 15:35:43 -0600

    Confirmacion 1 OK Mendieta
    Datos de desarrollo Kenya Mendieta
    28 de octubre 2024

---
+++ sección34MendietaED_pilas/src/sección34mendietaed_pilas/Sección34MendietaED_pilas.java
@@ -0,0 +1,25 @@
+
+package sección34mendietaed_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+
+public class Sección34MendietaED_pilas {
+
+    
+    public static void main(String[] args) {
+        // Declaracion de fila 
+        Stack<Integer> pilaMendieta =new  Stack<>();
+        
+        pilaMendieta.push(100);
+        pilaMendieta.push(150);
+        pilaMendieta.push(230);
+        pilaMendieta.push(10);
+        pilaMendieta.push(160);
+        
+        
+        JOptionPane.showMessageDialog(null,"Datos en pila ordenados es :\n"+pilaMendieta);
+    }
+    
+}
